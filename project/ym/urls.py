10cccd2d34f2c7ea4d0d7430cecf5b7d82f58f22
brucebett <bett6448@gmail.com>
2025-05-26 00:01:35 +0300

    mon  commit

--- project/ym/urls.py
+++ project/ym/urls.py
@@ -5,5 +5,6 @@ from ym import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
 
 ]
--- project/ym/views.py
+++ project/ym/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request, 'home.html')
 def contact(request):
     return render(request, 'contact.html')
+def service(request):
+    return render(request, 'service.html')
