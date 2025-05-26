0fd3eb4475112e9540774dce6629252575f4d24d
brucebett <bett6448@gmail.com>
2025-05-26 23:18:31 +0300

    mon23  commit

--- project/ym/views.py
+++ project/ym/views.py
@@ -7,3 +7,5 @@ def contact(request):
     return render(request, 'contact.html')
 def service(request):
     return render(request, 'service.html')
+def about(request):
+    return render(request, 'about.html')
